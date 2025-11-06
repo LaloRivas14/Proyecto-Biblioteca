61eacfd489b4a225fe4b4de2a41197eb8998b357
toledoelias <toledopabloelias@gmail.com>
2025-11-06 13:32:35 -0300

    Update Docente.java

--- Docente.java
+++ Docente.java
@@ -1,9 +1,9 @@
 
 /**
- * Write a description of class Docente here.
- * 
+ *La clase Docente es un subclase de la clase superclase (Socio).
+ * Esta clase modela a un Docente que a su vez es un Socio de la Biblioteca.
  * @author (pablo) 
- * @version (1)
+ * @version (1.1)
  */
 import java.util.Calendar;
 
@@ -12,9 +12,9 @@ public class Docente extends Socio {
     private String area;
     
     /**
-     * Constructor que crea un docente
-     * @param p_dniSocio dni del socio docente
-     * @param p_nombre nombre del docente
+     * Constructor para instanciar un objeto docente
+     * @param p_dniSocio dni del socio docente(heredado)
+     * @param p_nombre nombre del docente(heredado)
      * @param p_area area del docente
      */
     public Docente(int p_dniSocio, String p_nombre, String p_area) {
@@ -22,15 +22,22 @@ public class Docente extends Socio {
         this.setArea(p_area);
     }
     
+    /**setArea guarda el valor ingresado como parametro en la variable area*/
     private void setArea(String p_area) {
         this.area = p_area;
     }
-
+    /**@return area*/
     public String getArea() {
         return this.area;
     }
     
-     /**@return true si el docente es responsable */
+    /**
+     * Verifica si el docente es un socio responsable.
+     * Un docente es responsable si no tiene ningún préstamo vencido.
+     *
+     * @return true si el docente está al día (no tiene préstamos vencidos), 
+     * false si tiene al menos un préstamo vencido.
+     */
     public boolean esResponsable() {
         Calendar p_fecha = Calendar.getInstance();
         for (Prestamo unPrestamo : this.getPrestamos()) {
@@ -42,7 +49,7 @@ public class Docente extends Socio {
     }
     
      /**
-     * modifica setDiasPrestamo si el docente es responsable
+     * modifica setDiasPrestamo y le agrega dias si el docente es responsable
      * @param p_dias dias que se van a agregar
      */
     public void agregarDiasDePrestamo(int p_dias) {
@@ -51,7 +58,11 @@ public class Docente extends Socio {
         }
     }
     
-     /** @return "Docente"*/
+    /**
+     * Implementa el método abstracto 'soyDeLaClase' de la superclase Socio.
+     * @return la cadena "Docente" para identificar este tipo de socio.
+     */
+    @Override
     public String soyDeLaClase() {
         return "Docente";
     }
