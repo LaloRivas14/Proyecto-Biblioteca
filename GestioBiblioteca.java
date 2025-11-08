2ab863643ec6299c52e3177a68b19eecbf6a4103
Lautaro <lautarorivas17@gmail.com>
2025-11-08 10:21:49 -0300

    Modularizacion de los case, y aplicaciones de exepciones, InputMismatchException, NullPointerExeption y Exception

--- GestioBiblioteca.java
+++ GestioBiblioteca.java
@@ -28,9 +28,13 @@ public class GestioBiblioteca{
                     try{
                         cargarNuevoLibro(biblioteca);
                         break;
-                    }catch(Exception e){
-                        teclado.nextLine();
-                        System.out.println(" ERROR DE INGRESO DE DATO INCORRECTO : "+ e.getMessage());
+                    } catch (InputMismatchException e) {
+                        System.out.println(" ERROR DE INGRESO DE DATO INCORRECTO: " + e.getMessage());
+                        System.out.println(" PRESIONE ENTER PARA CONTINUAR");
+                        teclado.nextLine(); // Limpia el buffer
+                        break;
+                    } catch (Exception e) {
+                        System.out.println(" ERROR GENERAL: " + e.getMessage());
                         break;
                     }
 
@@ -38,78 +42,67 @@ public class GestioBiblioteca{
                     try{
                         cargarNuevoSocio(biblioteca);
                         break;
-                    }catch(Exception e){
-                        teclado.nextLine();
-                        System.out.println(" ERROR DE INGRESO DE DATO INCORRECTO : "+ e.getMessage());
+                    } catch (InputMismatchException e) {
+                        System.out.println(" ERROR DE INGRESO DE DATO INCORRECTO: " + e.getMessage());
+                        System.out.println(" PRESIONE ENTER PARA CONTINUAR");
+                        teclado.nextLine(); // Limpia el buffer
+                        break;
+                    } catch (Exception e) {
+                        System.out.println(" ERROR GENERAL: " + e.getMessage());
                         break;
                     }
 
                 case 3: 
-                    System.out.println("Seleccione una categoria para eliminar");
-                    System.out.println("Socio por DNI - 1");
-                    System.out.println("Libro - 2");
-                    System.out.print("Seleccionar una opcion: ");
-                    int tipoEliminar = teclado.nextInt();
-                    teclado.nextLine();
-
-                    if(tipoEliminar == 1){
-
-                        System.out.println("Ingrese el dni del socio a eliminar");
-                        int dniSocioEliminar = teclado.nextInt();
-                        teclado.nextLine();
-                        Socio quitarSocio = biblioteca.buscarSocio(dniSocioEliminar);
-                        if(quitarSocio != null){
-                            biblioteca.eliminarSocio(quitarSocio);
-                            System.out.println("El socio se ha eliminado correctamente");
-                        }else{
-                            System.out.println("No se encontro el socio");
-                        }
-
-                    }else if(tipoEliminar == 2){
-
-                        System.out.println(biblioteca.listaDeLibros());
-                        System.out.print("Elija el indice del libro que desea eliminar: ");
-                        int libroEliminar = teclado.nextInt();
-                        teclado.nextLine();
-                        if (libroEliminar > 0 && libroEliminar <= biblioteca.getLibros().size()) {
-                            Libro eliminarLibro = biblioteca.getLibros().get(libroEliminar - 1);
-                            if (eliminarLibro.prestado() == false) { 
-                                biblioteca.removerLibro(eliminarLibro); 
-                                System.out.println("El libro se ha eliminado correctamente");
-                            }else {
-                                System.out.println("El libro esta prestado");
-                            }
-                        } else {
-                            System.out.println("Ingrese un numero correcto");
-                        }
-
-                    } else {
-                        System.out.println("Seleccione una opcion correcta");
-                    }
-
-                    break;
-
-                case 4:
-                    Calendar fechaAct = Calendar.getInstance();
-
-                    System.out.print("ingrese el dni del socio: ");
-                    Socio socioPrestar = biblioteca.buscarSocio(teclado.nextInt());
-                    teclado.nextLine();
-                    System.out.print("Ingrese el titulo del libro: ");
-                    Libro libroPrestar = buscarLibro(teclado.nextLine(),biblioteca.getLibros());
-                    if(libroPrestar == null){
-                        System.out.println("Libro prestado o no encontrado");
+                    try{
+                        eliminacionDeElemento(biblioteca);
+                        break;
+                    } catch (InputMismatchException e) {
+                        System.out.println(" ERROR DE INGRESO DE DATO INCORRECTO: " + e.getMessage());
+                        System.out.println(" PRESIONE ENTER PARA CONTINUAR");
+                        teclado.nextLine(); // Limpia el buffer
+                        break;
+                    } catch (NullPointerException e) {
+                        System.out.println(" ERROR: Se intentó acceder a un objeto nulo. El socio no se encontró.");
+                        break;
+                    } catch (Exception e) {
+                        System.out.println(" ERROR GENERAL: " + e.getMessage());
                         break;
                     }
-                    biblioteca.prestarLibro(fechaAct,socioPrestar,libroPrestar);
-                    System.out.println("*** Libro Prestado Con Exito ***");
-                    break;
+
+                case 4:
+                    try{
+                        eliminacionDeElemento(biblioteca);
+                        break;
+                    } catch (InputMismatchException e) {
+                        System.out.println(" ERROR DE INGRESO DE DATO INCORRECTO: " + e.getMessage());
+                        System.out.println(" PRESIONE ENTER PARA CONTINUAR");
+                        teclado.nextLine(); // Limpia el buffer
+                        break;
+                    } catch (NullPointerException e) {
+                        System.out.println(" ERROR: Se intentó acceder a un objeto nulo. El socio no se encontró.");
+                        break;
+                    } catch (Exception e) {
+                        System.out.println(" ERROR GENERAL: " + e.getMessage());
+                        break;
+                    }
                 case 5:
-                    // falta verificacion ya que si  libroDevolver es null no se va a devolver nada va a causar exepciones
+                    try{
                     System.out.print("Ingrese el titulo del libro: ");
                     Libro libroDevolver = buscarLibro(teclado.nextLine(),biblioteca.getLibros());
                     biblioteca.devolverLibro(libroDevolver);
                     break;
+                    } catch (InputMismatchException e) {
+                        System.out.println(" ERROR DE INGRESO DE DATO INCORRECTO: " + e.getMessage());
+                        System.out.println(" PRESIONE ENTER PARA CONTINUAR");
+                        teclado.nextLine(); // Limpia el buffer
+                        break;
+                    } catch (NullPointerException e) {
+                        System.out.println(" ERROR: Se intentó acceder a un objeto nulo. El socio no se encontró.");
+                        break;
+                    } catch (Exception e) {
+                        System.out.println(" ERROR GENERAL: " + e.getMessage());
+                        break;
+                    }
                 case 6:
                     System.out.print("ingrese el tipo de socio que desea saber la cantidad: ");
                     String tipo = teclado.nextLine();
@@ -130,14 +123,18 @@ public class GestioBiblioteca{
                     try{
                         System.out.print("Ingrese el titulo del libro: ");
                         Libro libroEncontrar = buscarLibro(teclado.nextLine(),biblioteca.getLibros());
-                        if(libroEncontrar == null){
-                            System.out.println("Libro no encontrado ");
-                            break;
-                        }
                         System.out.println("El o los socios que tienen el libro: "+biblioteca.quienTieneElLibro(libroEncontrar));
                         break;
-                    }   catch(Exception e){
-                        System.out.println(" Error: " + e.getMessage());
+                    } catch (InputMismatchException e) {
+                        System.out.println(" ERROR DE INGRESO DE DATO INCORRECTO: " + e.getMessage());
+                        System.out.println(" PRESIONE ENTER PARA CONTINUAR");
+                        teclado.nextLine(); // Limpia el buffer
+                        break;
+                    } catch (NullPointerException e) {
+                        System.out.println(" ERROR: Se intentó acceder a un objeto nulo. El libro no se encontró.");
+                        break;
+                    } catch (Exception e) {
+                        System.out.println(" ERROR GENERAL: " + e.getMessage());
                         break;
                     }
 
@@ -162,59 +159,113 @@ public class GestioBiblioteca{
     public static void menuDeListas(){
         System.out.println("Menu de listas: \n 1)Docentes responsables \n 2)Prestamos Vencidos \n 3)Lista de socios \n 4)Lista de titulos \n 5)Lista de libros"); 
     }
-    
+
     public static void cargarNuevoLibro(Biblioteca p_b){
         Scanner teclado = new Scanner(System.in);
         System.out.print("Ingrese el titulo del libro: ");
-                        String tituloLibro = teclado.nextLine();
-                        System.out.print("Ingrese el num de edicion del libro: ");
-                        int edicionLibro = teclado.nextInt();
-                        teclado.nextLine();
-                        System.out.print("Ingrese la editorial del libro: ");
-                        String editorialLibro = teclado.nextLine();
-                        System.out.print("ingrese el año del libro: ");
-                        int anioLibro = teclado.nextInt();
-                        teclado.nextLine();
-                        p_b.nuevoLibro(tituloLibro,edicionLibro,editorialLibro,anioLibro);
-                        System.out.println("**** Ingreso de libro exitoso ****");
+        String tituloLibro = teclado.nextLine();
+        System.out.print("Ingrese el num de edicion del libro: ");
+        int edicionLibro = teclado.nextInt();
+        teclado.nextLine();
+        System.out.print("Ingrese la editorial del libro: ");
+        String editorialLibro = teclado.nextLine();
+        System.out.print("ingrese el año del libro: ");
+        int anioLibro = teclado.nextInt();
+        teclado.nextLine();
+        p_b.nuevoLibro(tituloLibro,edicionLibro,editorialLibro,anioLibro);
+        System.out.println("**** Ingreso de libro exitoso ****");
     }
-    
+
     public static void cargarNuevoSocio( Biblioteca p_b){
         Scanner teclado = new Scanner(System.in);
         System.out.println("Que tipo de Socio desea agregar: ");
-                    System.out.println("Estudiante - 1");
-                    System.out.println("Docente - 2");
-                    System.out.println("Selecciona una opcion: ");
-                    int tipoS = teclado.nextInt();
-                    teclado.nextLine();
+        System.out.println("Estudiante - 1");
+        System.out.println("Docente - 2");
+        System.out.println("Selecciona una opcion: ");
+        int tipoS = teclado.nextInt();
+        teclado.nextLine();
 
-                    if(tipoS == 1){
+        if(tipoS == 1){
 
-                        System.out.println("Ingrese el dni del estudiante: ");
-                        int dniEstudiante = teclado.nextInt();
-                        teclado.nextLine();
-                        System.out.println("Ingrese el nombre y apellido del estudiante: ");
-                        String nombreEstudiante = teclado.nextLine();
-                        System.out.println("Ingrese la carrera del estudiante: ");
-                        String carreraEstudiante = teclado.nextLine();
+            System.out.println("Ingrese el dni del estudiante: ");
+            int dniEstudiante = teclado.nextInt();
+            teclado.nextLine();
+            System.out.println("Ingrese el nombre y apellido del estudiante: ");
+            String nombreEstudiante = teclado.nextLine();
+            System.out.println("Ingrese la carrera del estudiante: ");
+            String carreraEstudiante = teclado.nextLine();
 
-                        p_b.nuevoSocioEstudiante(dniEstudiante,nombreEstudiante, carreraEstudiante);
-                        System.out.println("Ingreso del estudiante exitoso");
+            p_b.nuevoSocioEstudiante(dniEstudiante,nombreEstudiante, carreraEstudiante);
+            System.out.println("Ingreso del estudiante exitoso");
 
-                    }else if(tipoS == 2){
-                        System.out.println("Ingrese el dni del docente: ");
-                        int dniDocente = teclado.nextInt();
-                        teclado.nextLine();
-                        System.out.println("Ingrese el nombre y apellido del docente: ");
-                        String nombreDocente = teclado.nextLine();
-                        System.out.println("Ingrese el area del docente: ");
-                        String areaDocente = teclado.nextLine();
+        }else if(tipoS == 2){
+            System.out.println("Ingrese el dni del docente: ");
+            int dniDocente = teclado.nextInt();
+            teclado.nextLine();
+            System.out.println("Ingrese el nombre y apellido del docente: ");
+            String nombreDocente = teclado.nextLine();
+            System.out.println("Ingrese el area del docente: ");
+            String areaDocente = teclado.nextLine();
 
-                        p_b.nuevoSocioDocente(dniDocente,nombreDocente, areaDocente);
-                        System.out.println("Ingreso del docente exitoso");
-                    }else{
-                        System.out.println("Seleccione una opcion correcta: ");
-                    }
+            p_b.nuevoSocioDocente(dniDocente,nombreDocente, areaDocente);
+            System.out.println("Ingreso del docente exitoso");
+        }else{
+            System.out.println("Seleccione una opcion correcta: ");
+        }
+    }
+
+    public static void prestamoLibro(Biblioteca p_b){
+        Scanner teclado = new Scanner(System.in);
+        Calendar fechaAct = Calendar.getInstance();
+
+        System.out.print("ingrese el dni del socio: ");
+        Socio socioPrestar = p_b.buscarSocio(teclado.nextInt());
+        teclado.nextLine();
+        System.out.print("Ingrese el titulo del libro: ");
+        Libro libroPrestar = buscarLibro(teclado.nextLine(),p_b.getLibros());
+        p_b.prestarLibro(fechaAct,socioPrestar,libroPrestar);
+        System.out.println("*** Libro Prestado Con Exito ***");
+    }
+
+    public static void eliminacionDeElemento(Biblioteca p_b){
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Seleccione una categoria para eliminar");
+        System.out.println("Socio por DNI - 1");
+        System.out.println("Libro - 2");
+        System.out.print("Seleccionar una opcion: ");
+        int tipoEliminar = teclado.nextInt();
+        teclado.nextLine();
+
+        if(tipoEliminar == 1){
+
+            System.out.println("Ingrese el dni del socio a eliminar");
+            int dniSocioEliminar = teclado.nextInt();
+            teclado.nextLine();
+            Socio quitarSocio = p_b.buscarSocio(dniSocioEliminar);
+            p_b.eliminarSocio(quitarSocio);
+            System.out.println("El socio se ha eliminado correctamente");
+
+        }else if(tipoEliminar == 2){
+            System.out.println(p_b.listaDeLibros());
+            System.out.print("Elija el indice del libro que desea eliminar: ");
+            int libroEliminar = teclado.nextInt();
+            teclado.nextLine();
+            if (libroEliminar > 0 && libroEliminar <= p_b.getLibros().size()) {
+                Libro eliminarLibro = p_b.getLibros().get(libroEliminar - 1);
+                if (eliminarLibro.prestado() == false) { 
+                    p_b.removerLibro(eliminarLibro); 
+                    System.out.println("El libro se ha eliminado correctamente");
+                }else {
+                    System.out.println("El libro esta prestado");
+                }
+            } else {
+                System.out.println("Ingrese un numero correcto");
+            }
+
+        } else {
+            System.out.println("Seleccione una opcion correcta");
+        }
+
     }
 
     public static void mostrarUnaLista(int opcion,Biblioteca p_b){
